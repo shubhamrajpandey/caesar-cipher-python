bcc14c0029e67262cba4c260f97f01abbf137234
[Shubham Pandey] <shubhamrajpandey875@gmail.com>
2025-05-20 21:46:14 +0545

    Added Welcome Message in the beginning

---
+++ caesar-cipher.py
@@ -0,0 +1,4 @@
+def welcome():
+    # This function prints a welcome message for the Caesar Cipher program.
+    print("Welcome to the Caesar Cipher\n"
+          "This program encrypts and decrypts text using the Caesar Cipher.")
